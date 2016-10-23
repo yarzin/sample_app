8f3c8eb5fd134d307f9dc0a4495503e40252c054
Yaroslav Zin <zinyaroslav@gmail.com>
2016-10-23 22:49:38 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root             'static_pages#home'
 
+
+
+
+  root             'static_pages#home'
+  get 'signup'  => 'users#new'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
